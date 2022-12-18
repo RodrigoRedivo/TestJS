4aa5ddc2c0d0709fa5151e012f6a5ccab84ce3b7
RodrigoRedivo <rodrigoredivo@pop-os.localdomain>
2022-12-18 18:42:12 -0300

    refactor: Mais refactoring com suporte dos testes

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,10 +1,9 @@
-module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Please check yout params');
-      }
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check yout params');
+  }
+  return `${key}=${value}`;
+};
 
-      return `${key}=${value}`;
-    })
-    .join('&');
+module.exports.queryString = obj =>
+  Object.entries(obj).map(keyValueToString).join('&');
