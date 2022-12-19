3407c84c47c13e4876a94477d6ce3890ba64a2d8
RodrigoRedivo <rodrigoredivo@pop-os.localdomain>
2022-12-19 09:31:25 -0300

    test: Implementar com TDD o método para fazer parse da Query String - parte 1

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provider', () => {
@@ -31,5 +31,24 @@ describe('Object to query string', () => {
     expect(() => {
       queryString(obj);
     }).toThrowError();
+  });
+});
+
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Fabio&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Fabio',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a query string of a single key=value to object', () => {
+    const qs = 'name=Fabio';
+
+    expect(parse(qs)).toEqual({
+      name: 'Fabio',
+    });
   });
 });
